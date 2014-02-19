df8231311e0ff7422e22162494200924e477a5af
Jeremy W. Rowe <jeremy.w.rowe@gmail.com>
2014-02-18 22:37:39 -0500

    uses new cors endpoint to create signups

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,17 +1,21 @@
 $(function() {
   $( "#signup_form" ).submit(function(e) {
-    event.preventDefault();
+    e.preventDefault();
+    console.log($("#signup_form").serialize());
     $.ajax({
       url: $("#signup_form").attr('action'),
-      type: "post",
-      data: $("#signup_form").serialize(),
-      dataType: "js",
+      type: "POST",
       crossDomain: true,
-      headers: { "X-Requested-With": "XMLHttpRequest" },
-      success: function(data)
-      {
-        console.log(data);
-      } 
+      dataType: "json",
+      data: $("#signup_form").serialize(),
+      error: function(xhr, textStatus, error) {
+        console.dir(JSON.parse(xhr.responseText));
+        console.log(textStatus);
+        console.log(error);
+      },
+      success: function(data) {
+        console.dir(data);
+      }
     });
   });
 });
